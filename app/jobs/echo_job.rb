ecba7a9b48ec552171b5154634025adc279c13eb
Radamanthus Batnag <radamanthus@gmail.com>
2017-03-02 14:59:16 +0800

    Modify echo_job to throw an output to STDOUT

--- app/jobs/echo_job.rb
+++ app/jobs/echo_job.rb
@@ -2,6 +2,7 @@ class EchoJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts "DEBUG: #{Time.now} This is an output to STDOUT. It will cause Sidekiq to throw a Broken Pipe error."
     `echo "ECHO JOB: #{self.object_id} | #{Time.now}" >> #{File.join(Rails.root, 'tmp', 'echo.output')}`
   end
 end
