8290a1314dcdf654e08deb5b4f134d0bcff4b5a5
Radamanthus Batnag <radamanthus@gmail.com>
2017-03-31 09:42:24 +0800

    Switch to DelayedJob

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module RailsActivejobExample
   class Application < Rails::Application
     # Enable/disable these lines to switch the ActiveJob backend
     # Only one backend at a time can be enabled
-    config.active_job.queue_adapter = :sidekiq
-    #config.active_job.queue_adapter = :delayed_job
+    #config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :delayed_job
     #config.active_job.queue_adapter = :resque
   end
 end
