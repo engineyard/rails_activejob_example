d3c860864675a33ba9615f4603edd1d44d614a65
Radamanthus Batnag <radamanthus@gmail.com>
2017-04-23 14:53:34 +0800

    Add redis url to sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,7 @@ sentinel_config = YAML.load_file(Rails.root + 'config/redis-sentinel.yml')
 sentinels = sentinel_config['sentinels'].map{ |s| "#{s}:#{sentinel_config['port']}" }
 Sidekiq.configure_client do |config|
   config.redis = {
+    url: "redis://redis",
     master_name: 'redis',
     sentinels: sentinels,
     failover_reconnect_timeout: 20,      # roughly 3-5 seconds longer than the sentinel failover-timeout
@@ -10,6 +11,7 @@ Sidekiq.configure_client do |config|
 end
 Sidekiq.configure_server do |config|
   config.redis = {
+    url: "redis://redis",
     master_name: 'redis',
     sentinels: sentinels,
     failover_reconnect_timeout: 20,      # roughly 3-5 seconds longer than the sentinel failover-timeout
