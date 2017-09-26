6e99ebb198a8eb813b5f504462c1e81b133d9950
Radamanthus Batnag <radamanthus@gmail.com>
2017-09-26 14:48:28 +0800

    Add debug line

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,9 @@
 on_utilities() do
   # Run only on instances named sidekiq*
   if /^sidekiq/.match config.current_name
-    info("Here!")
     worker_count = 1 # please replace your own value
     (0...worker_count-1).each do |i|
+      info("Terminating worker #{i}...")
       sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
     end
   end
