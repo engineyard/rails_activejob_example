39c9383125bb648bca6e6c60ee4e0e866327f8fb
Radamanthus Batnag <radamanthus@gmail.com>
2017-09-26 14:37:37 +0800

    Add debug line to deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,10 +1,10 @@
-on_utilities() do
+on_utilities("sidekiq") do
   info("Running on #{config.current_name}")
   # Run only on instances named sidekiq*
-  if /^sidekiq/.match config.current_name
+  #if /^sidekiq/.match config.current_name
     worker_count = 1 # please replace your own value
     (0...worker_count-1).each do |i|
       "sudo kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
     end
-  end
+  #end
 end
