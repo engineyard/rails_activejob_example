727f9b341c3cb6c5fac89066cd5f08432b0198ba
Radamanthus Batnag <radamanthus@gmail.com>
2017-09-26 15:00:20 +0800

    hard code worker id

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,11 +1,5 @@
 on_utilities() do
-  # Run only on instances named sidekiq*
   if /^sidekiq/.match config.current_name
-    info("Here!")
-    worker_count = 1 # please replace your own value
-    (0...worker_count-1).to_a.each do |i|
-      info("Terminating worker #{i}...")
-      sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
-    end
+    sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_0.pid`"
   end
 end
--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,9 +1,3 @@
 on_utilities() do
-  # Run only on instances named sidekiq*
-  if /^sidekiq/.match config.current_name
-    worker_count = 1 # please replace your own value
-    (0...worker_count-1).each do |i|
-      sudo "kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
-    end
-  end
+  sudo "kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_0.pid`"
 end
