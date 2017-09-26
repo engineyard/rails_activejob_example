64fef760036f9b3b49072e591d7db27be34690a0
Radamanthus Batnag <radamanthus@gmail.com>
2017-09-26 15:09:31 +0800

    more hook tests

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,8 @@
 on_utilities() do
   if /^sidekiq/.match config.current_name
-    sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_0.pid`"
+    worker_count = 1
+    (0..worker_count).each do |i|
+      sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
+    end
   end
 end
--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,3 +1,8 @@
 on_utilities() do
-  sudo "kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_0.pid`"
+  if /^sidekiq/.match config.current_name
+    worker_count = 1
+    (0..worker_count).each do |i|
+      sudo "kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
+    end
+  end
 end
