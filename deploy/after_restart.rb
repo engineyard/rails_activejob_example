ea9da1e67a75caab8d5aafdb477166fa39a8184f
Radamanthus Batnag <radamanthus@gmail.com>
2017-09-26 14:42:11 +0800

    Fix deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,10 +1,9 @@
-on_utilities("sidekiq") do
-  info("Running on #{config.current_name}")
+on_utilities() do
   # Run only on instances named sidekiq*
-  #if /^sidekiq/.match config.current_name
+  if /^sidekiq/.match config.current_name
     worker_count = 1 # please replace your own value
     (0...worker_count-1).each do |i|
-      "sudo kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
+      sudo "kill -TERM `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
     end
-  #end
+  end
 end
--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,9 +1,9 @@
-on_utilities("sidekiq") do
+on_utilities() do
   # Run only on instances named sidekiq*
-  #if /^sidekiq/.match config.current_name
+  if /^sidekiq/.match config.current_name
     worker_count = 1 # please replace your own value
-    (0...worker_count).each do |i|
-      "sudo kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
+    (0...worker_count-1).each do |i|
+      sudo "kill -USR1 `cat /var/run/engineyard/sidekiq/rails_activejob_example/sidekiq_#{i}.pid`"
     end
-  #end
+  end
 end
