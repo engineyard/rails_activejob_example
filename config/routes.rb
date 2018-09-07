c44f1a50f92072c0c7bda29232fbad8124029ac3
Johann Fuechsl <johann@fuechsl.co>
2018-09-07 12:55:27 +0200

    Mount the Sidekiq Web UI to the /sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+require 'sidekiq/web'
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
 end
