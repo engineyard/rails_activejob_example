70c848c6f85f9994b8ca32b74d7f62ad7b00e86e
Johann Fuechsl <johann@fuechsl.co>
2018-09-11 10:15:09 +0200

    Add redis and sidekiq custom cookbooks. Sidekiq is configured to not run workers.

---
+++ cookbooks/custom-sidekiq/attributes/default.rb
@@ -0,0 +1,18 @@
+# More information on how to use this file to customize the Sidekiq cookbook is
+# available in custom_sidekiq/README.md
+#
+# this is the default
+# default['sidekiq']['is_sidekiq_instance'] = true
+
+# run the recipe on a utility instance named background_workers
+# default['sidekiq']['is_sidekiq_instance'] = (node['dna']['instance_role'] == 'util' && node['dna']['name'] == 'background_workers')
+
+# run the recipe on a solo instance
+# default['sidekiq']['is_sidekiq_instance'] = (node['dna']['instance_role'] == 'solo')
+
+# Default memory limit
+# default['sidekiq']['worker_memory'] = 400
+
+# don't run sidekiq workers (we will run them as a container service)
+default['sidekiq']['is_sidekiq_instance'] = false
+default['sidekiq']['workers'] = 0
